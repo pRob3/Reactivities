78bae7cc3a8115a893cdd6e8036214f2ccb88bf1
Dennis Asplind <skene@hotmail.com>
2023-05-11 18:13:00 +0200

    added error page and installed toastify

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -14,6 +14,7 @@ export default function NavBar() {
           Reactivities
         </Menu.Item>
         <Menu.Item name='Activities' as={NavLink} to='/activities' />
+        <Menu.Item name='Errors' as={NavLink} to='/errors' />
         <Menu.Item>
           <Button
             as={NavLink}
