441252b968f8358cae84e995f0054ee091e4bb62
Dennis Asplind <skene@hotmail.com>
2023-06-09 18:52:48 +0200

    signalR on the client

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -29,7 +29,11 @@ public static class ApplicationServiceExtensions
         {
             opt.AddPolicy("CorsPolicy", policy =>
             {
-                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                policy
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:3000");
             });
         });
         services.AddMediatR(typeof(List.Handler));
